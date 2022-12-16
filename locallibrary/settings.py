d484d9d52974384906cfe9eb300c45666d03a863
razin777 <97068022+razin777@users.noreply.github.com>
2022-12-16 16:41:07 +0200

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -161,3 +161,13 @@ ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
 # ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
+
+
+
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
