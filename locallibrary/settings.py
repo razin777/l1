69b9a61b2a4d3624badd80bfce6ebf2f79e60bbe
razin777 <97068022+razin777@users.noreply.github.com>
2022-12-16 17:54:51 +0200

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -157,7 +157,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ## For example, for a site URL at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
 
-ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['illegal-agreement-production.up.railway.app', '127.0.0.1']
 
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
